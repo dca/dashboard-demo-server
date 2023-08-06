c460f7923d4611354d0f1ffdae68e14595e82bc1
Dca <dcahsu@gmail.com>
2023-08-06 23:40:44 +0800

    feat: Add libs/db, user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DbModule } from '@app/db';
 
 @Module({
-  imports: [],
+  imports: [
+    DbModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
