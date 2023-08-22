fdff06839a57b0d63f5a43c8ec529a8e1a48dc13
Dca <dcahsu@gmail.com>
2023-08-22 13:55:05 +0800

    test: Fix all test

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserController } from './user.controller'
+import { UserService } from './user.service'
 
 describe('UserController', () => {
   let controller: UserController
+  let mockUserService: Partial<UserService>
 
   beforeEach(async () => {
+    mockUserService = {
+      createUser: jest.fn(),
+      getAllUsers: jest.fn(),
+      getUserById: jest.fn(),
+      getActiveSessionsToday: jest.fn(),
+      getAverageActiveSessionsLast7Days: jest.fn()
+    }
+
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController]
+      controllers: [UserController],
+      providers: [
+        { provide: UserService, useValue: mockUserService }
+      ]
     }).compile()
 
     controller = module.get<UserController>(UserController)
